6d6b21c051ce8884e6e667b59dbdb83eefa2efde
CorgiChen <qq882517@gmail.com>
2023-05-18 00:56:08 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 "# smart.laundry.io" 
-"# smart.laundry.io" 
---
+++ doc.js
@@ -0,0 +1,48 @@
+var flag1 = false, flag2 = true, flag3 = true, flag4 = true;
+var m1 = document.getElementById("Machine1");
+if (flag1)
+{
+    m1.innerText = "Finished";
+    m1.style.color = "green";
+}
+else
+{
+    m1.innerText = "Unfinished";
+    m1.style.color = "red";
+}
+
+var m2 = document.getElementById("Machine2");
+if (flag2)
+{
+    m2.innerText = "Finished";
+    m2.style.color = "green";
+}
+else
+{
+    m2.innerText = "Unfinished";
+    m2.style.color = "red";
+}
+
+var m3 = document.getElementById("Machine3");
+if (flag3)
+{
+    m3.innerText = "Finished";
+    m3.style.color = "green";
+}
+else
+{
+    m3.innerText = "Unfinished";
+    m3.style.color = "red";
+}
+
+var m4 = document.getElementById("Machine4");
+if (flag4)
+{
+    m4.innerText = "Finished";
+    m4.style.color = "green";
+}
+else
+{
+    m4.innerText = "Unfinished";
+    m4.style.color = "red";
+}
